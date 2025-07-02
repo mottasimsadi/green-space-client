b354d267049597dcf99d498d34fb478d4ef77154
mottasimsadi <mottasimsadi@gmail.com>
2025-07-03 05:26:27 +0600

    Solved the chunk size limit issue while building

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if(id.includes('node_modules')) {
+            return id
+            .split('node_modules/')[1]
+            .split('/')[0]
+            .toString();
+          }
+        }
+      }
+    }
+  }
 })
